7330aae7f3ba81d88c0640de61aa7124ff86f715
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-04-05 22:50:50 +0530

    ( Dummy Structure got ready)
    I have just complete the dummy structure with TODO: List

---
+++ lib/view/screen/course_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class CourseScreen extends StatefulWidget {
+  const CourseScreen({super.key});
+
+  @override
+  State<CourseScreen> createState() => _CourseScreenState();
+}
+
+class _CourseScreenState extends State<CourseScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        // TODO: Making Exactly like course screen Shown in ClipherSchool
+        appBar: AppBar(
+          title: const Text('ClipherSchool'),
+        ),
+        body: Column(
+          children: [
+            ListView(
+              // TODO: Making Exactly like course screen Shown in ClipherSchool and with auto changing feature
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.6,
+                  width: double.infinity,
+                  color: Colors.red,
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.6,
+                  width: double.infinity,
+                  color: Colors.red,
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.6,
+                  width: double.infinity,
+                  color: Colors.red,
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.6,
+                  width: double.infinity,
+                  color: Colors.red,
+                ),
+              ],
+            ),
+            Row(
+              // TODO: Complete this Row
+              children: [
+                const Text("Recommended Courses"),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Popular >'),
+                ),
+              ],
+            ),
+            Row(
+              // TODO: Complete this Row
+              children: [
+                const Text("Latest Courses"),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Popular >'),
+                ),
+              ],
+            ),
+            Row(
+              // TODO: Complete this Row
+              children: [
+                const Text("Popular Categories"),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Popular >'),
+                ),
+              ],
+            ),
+            Row(
+              // TODO: Complete this Row
+              children: [
+                const Text('All Courses'),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Popular >'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/view/screen/home_screen.dart
+++ lib/view/screen/home_screen.dart
@@ -10,8 +10,105 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Scaffold(),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            // TODO: Adding Logo of ClipherSchool and cloning look remains
+            children: const [
+              Icon(Icons.home),
+              Text('ClipherSchool'),
+            ],
+          ),
+          backgroundColor: Colors.white,
+          actions: [
+            // TODO: Adding A Drawer Look
+            IconButton(
+              icon: const Icon(Icons.drafts),
+              onPressed: () {},
+            ),
+          ],
+          centerTitle: false,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              // TODO: Cloning Text and animation remains  make a package
+              const Text(
+                'Welcome to the Future of Learning!',
+              ),
+              const Text(
+                'Start Learning by best creators for absolutely free!',
+              ),
+              Row(
+                children: [
+                  Row(
+                    children: [
+                      // TODO: Adding Images exactly and make a pakage
+                      const CircleAvatar(),
+                      const CircleAvatar(),
+                      const CircleAvatar(),
+                      Column(
+                        children: const [
+                          Text("50+"),
+                          Text("Mentors"),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          // TODO: Adding rating in svg file and cloning look remains
+                          const Text("4.8/5"),
+                          Row(
+                            children: const [
+                              Icon(Icons.star),
+                              Icon(Icons.star),
+                              Icon(Icons.star),
+                              Icon(Icons.star),
+                              Icon(Icons.star),
+                              Text("Ratings"),
+                            ],
+                          ),
+                          // TODO: Cloning Look
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "Start Learning Now ->",
+                            ),
+                          ),
+                          // TODO: No package required just make animated crolling looks
+                          Container(
+                            height: 100,
+                            width: 100,
+                            color: Colors.black,
+                          ),
+                          Row(
+                            // TODO: Excactly copying look
+                            children: const [
+                              Text("15K +"),
+                              Text("Students"),
+                            ],
+                          ),
+                          // TODO: Can made a package for this half skill
+                          Container(
+                            height: MediaQuery.of(context).size.height * 0.5,
+                            width: double.infinity,
+                            color: Colors.black,
+                          ),
+                          Container(
+                            height: MediaQuery.of(context).size.height * 0.5,
+                            width: double.infinity,
+                            color: Colors.black,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
