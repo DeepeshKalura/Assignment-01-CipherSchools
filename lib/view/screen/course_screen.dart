2c2a44477ca54e1f967afb52c2d7448ef4d88cfc
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-04-07 17:29:40 +0530

    (ADD) --> Complete hero parts

--- lib/view/screen/course_screen.dart
+++ lib/view/screen/course_screen.dart
@@ -1,4 +1,8 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+
+import '../widget/course_container_widget.dart';
+import '../widget/myappbar_widget.dart';
 
 class CourseScreen extends StatefulWidget {
   const CourseScreen({super.key});
@@ -8,84 +12,110 @@ class CourseScreen extends StatefulWidget {
 }
 
 class _CourseScreenState extends State<CourseScreen> {
+  final List<Widget> courseContainerList = [
+    const CoursesContainerWidget(),
+    const CoursesContainerWidget(),
+    const CoursesContainerWidget(),
+    const CoursesContainerWidget()
+  ];
+
+  final List<Color> _indicatorColors = [
+    Colors.orange,
+    Colors.orange,
+    Colors.orange,
+    Colors.orange,
+  ];
+
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        // TODO: Making Exactly like course screen Shown in ClipherSchool
-        appBar: AppBar(
-          title: const Text('ClipherSchool'),
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: ListView(
-                // TODO: Making Exactly like course screen Shown in ClipherSchool and with auto changing feature
+        appBar: const MyAppBarWidget(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
                 children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.6,
-                    width: double.infinity,
-                    color: Colors.red,
+                  CarouselSlider(
+                    options: CarouselOptions(
+                      autoPlay: true,
+                      height: MediaQuery.of(context).size.height * 0.52,
+                      autoPlayInterval: const Duration(seconds: 5),
+                      enableInfiniteScroll: false,
+                      viewportFraction: 1.0,
+                      onPageChanged: (index, _) {
+                        setState(() {
+                          _currentIndex = index;
+                        });
+                      },
+                    ),
+                    items: courseContainerList,
                   ),
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.6,
-                    width: double.infinity,
-                    color: Colors.red,
-                  ),
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.6,
-                    width: double.infinity,
-                    color: Colors.red,
-                  ),
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.6,
-                    width: double.infinity,
-                    color: Colors.red,
+                  Positioned(
+                    bottom: MediaQuery.of(context).size.height * 0.07,
+                    left: 0,
+                    right: 0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                        _indicatorColors.length,
+                        (index) => Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: CircleAvatar(
+                            radius: 4,
+                            backgroundColor: index == _currentIndex
+                                ? _indicatorColors[_currentIndex]
+                                : Colors.grey[300],
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
-            ),
-            Row(
-              // TODO: Complete this Row
-              children: [
-                const Text("Recommended Courses"),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Popular >'),
-                ),
-              ],
-            ),
-            Row(
-              // TODO: Complete this Row
-              children: [
-                const Text("Latest Courses"),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Popular >'),
-                ),
-              ],
-            ),
-            Row(
-              // TODO: Complete this Row
-              children: [
-                const Text("Popular Categories"),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Popular >'),
-                ),
-              ],
-            ),
-            Row(
-              // TODO: Complete this Row
-              children: [
-                const Text('All Courses'),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Popular >'),
-                ),
-              ],
-            ),
-          ],
+              Row(
+                // TODO: Complete this Row
+                children: [
+                  const Text("Recommended Courses"),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Popular >'),
+                  ),
+                ],
+              ),
+              Row(
+                // TODO: Complete this Row
+                children: [
+                  const Text("Latest Courses"),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Popular >'),
+                  ),
+                ],
+              ),
+              Row(
+                // TODO: Complete this Row
+                children: [
+                  const Text("Popular Categories"),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Popular >'),
+                  ),
+                ],
+              ),
+              Row(
+                // TODO: Complete this Row
+                children: [
+                  const Text('All Courses'),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Popular >'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
