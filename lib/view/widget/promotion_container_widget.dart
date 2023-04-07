96f494359741039b19f620b25cb247ba6fc5ac04
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-04-07 10:26:58 +0530

    (ADD) --> File
    (WHY) ->This is file is created to become a animated Widget
    
    (WHAT) -> It is has a color container with data presented in good manner
    
    (PROBLEM) -> Currently work fine but need some modification.

---
+++ lib/view/widget/promotion_container_widget.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class PromotionContainerWidget extends StatelessWidget {
+  final String firstText;
+  final String secondText;
+  final IconData iconData;
+
+  const PromotionContainerWidget({
+    super.key,
+    required this.firstText,
+    required this.secondText,
+    required this.iconData,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color.fromRGBO(53, 58, 66, 1),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.02,
+                  left: MediaQuery.of(context).size.width * 0.05,
+                ),
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    iconData,
+                    color: Colors.orange,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.02,
+                ),
+                color: Colors.orange,
+                padding: const EdgeInsets.symmetric(
+                  vertical: 4.0,
+                  horizontal: 8.0,
+                ),
+                child: const Text(
+                  'FREE',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: MediaQuery.of(context).size.height * 0.02,
+              left: MediaQuery.of(context).size.width * 0.05,
+              bottom: MediaQuery.of(context).size.height * 0.005,
+            ),
+            child: Text(
+              firstText,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 26.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: MediaQuery.of(context).size.width * 0.05,
+              bottom: MediaQuery.of(context).size.height * 0.02,
+            ),
+            child: Text(
+              secondText,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 14.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
