2c2b9f809763c6f7d0d39fff4be642caee0fcebf
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-04-07 07:23:04 +0530

    (Complete) --> 2 parts of UI
    It consist of three parts.
    --> Animated Text which code I completed with package,
    --> RatingsRowWidget
    it has a complete row UI.
    --> An elevated button
    
    all looks a little off But don't worry it fine with whitespaces

--- lib/view/screen/home_screen.dart
+++ lib/view/screen/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:typing_animation/typing_animation.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../widget/ratings_row_widget.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -56,66 +58,97 @@ class _HomeScreenState extends State<HomeScreen> {
         body: SingleChildScrollView(
           child: Column(
             children: [
-              // TODO: Cloning Text and animation remains  make a package
-              const Text(
-                'Welcome to the Future of Learning!',
-              ),
-              const Text(
-                'Start Learning by best creators for absolutely free!',
-              ),
-              Row(
-                children: [
-                  Row(
-                    children: [
-                      // TODO: Adding Images exactly and make a pakage
-                      const CircleAvatar(),
-                      const CircleAvatar(),
-                      const CircleAvatar(),
-                      Column(
-                        children: const [
-                          Text("50+"),
-                          Text("Mentors"),
-                        ],
+              Padding(
+                padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height / 10,
+                  left: 20,
+                  right: 20,
+                ),
+                child: RichText(
+                  textAlign: TextAlign.center,
+                  text: const TextSpan(
+                    style: TextStyle(
+                      fontSize: 42.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'Welcome to ',
                       ),
-                      Column(
-                        children: [
-                          // TODO: Adding rating in svg file and cloning look remains
-                          const Text("4.8/5"),
-                          Row(
-                            children: const [
-                              Icon(Icons.star),
-                              Icon(Icons.star),
-                              Icon(Icons.star),
-                              Icon(Icons.star),
-                              Icon(Icons.star),
-                              Text("Ratings"),
-                            ],
-                          ),
-                          // TODO: Cloning Look
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: const Text(
-                              "Start Learning Now ->",
-                            ),
-                          ),
-                          // TODO: No package required just make animated crolling looks
-                          Container(
-                            height: 100,
-                            width: 100,
-                            color: Colors.black,
-                          ),
-                          Row(
-                            // TODO: Excactly copying look
-                            children: const [
-                              Text("15K +"),
-                              Text("Students"),
-                            ],
-                          ),
-                          // TODO: Can made a package for this half skill
-                        ],
+                      TextSpan(
+                        text: 'the Future',
+                        style: TextStyle(
+                          color: Colors.orange,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                        text: ' of Learning!',
                       ),
                     ],
                   ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height / 23,
+                  left: 10,
+                  right: 10,
+                ),
+                child: Wrap(
+                  alignment: WrapAlignment.center,
+                  children: [
+                    Text(
+                      'Start Learning by best creators for ',
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.grey[800],
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const TypingAnimation(
+                      text: 'absolutely free ',
+                      textStyle: TextStyle(
+                        color: Colors.orange,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const RatingsRowWidget(),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.orange),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  elevation: MaterialStateProperty.all(0),
+                ),
+                child: const Text(
+                  "Start Learning Now ->",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.black,
+              ),
+              Row(
+                // TODO: Excactly copying look
+                children: const [
+                  Text("15K +"),
+                  Text("Students"),
                 ],
               ),
             ],
