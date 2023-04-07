ae4ba37786c77e8925004722902994f838856c08
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-04-07 10:21:54 +0530

    (ADD) --> FIle
    It is a model file which provides blueprint for  PromtionContainerWidget
    
    The file most higher chance of modification or deletion

---
+++ lib/model/parameter_of_promotion_container_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class ParameterOfPromotionContainerModel {
+  final String firstText;
+  final String secondText;
+  final IconData iconData;
+
+  ParameterOfPromotionContainerModel({
+    required this.firstText,
+    required this.secondText,
+    required this.iconData,
+  });
+}
